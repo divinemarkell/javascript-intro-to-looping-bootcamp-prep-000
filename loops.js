13baa66fbce8fbd44cc19159d613708e4fb67c65
Markell Owens <markellowens@mac.com>
2018-08-22 17:57:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,5 +16,14 @@ function whileLoop (n) {
   return 'done';
 }
 function doWhileLoop (array) {
-  
+  var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+do {
+  console.log('doo-bee-doo-bee-doo')
+  incrementVariable();
+} while (i < 5);
 }
